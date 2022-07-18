494402ce86edb4a473e818e926d03e6b9bd89f40
Nogtif <quentin.cptr@gmail.com>
2022-07-18 23:28:22 +0200

    🚛 daily commit

--- src/views/Anime/Anime.tsx
+++ src/views/Anime/Anime.tsx
@@ -9,6 +9,9 @@ export default defineComponent({
     const loadAnime = async () => {
       anime.value = await fetchAnime(`one-piece`).then((res) => res)
     }
+
+    const nbOfStars = (rating: number) => Math.floor(rating / 20);
+
     loadAnime()
 
     return () => (anime.value != null && 
@@ -19,8 +22,26 @@ export default defineComponent({
 
             <aside>
               <h1>{ anime.value.title }</h1>
+              <div class="rating">
+                { 
+                  [...Array(5).keys()].map((star) => <i class={
+                    ['mdi', nbOfStars(anime.value.rating) < star ? 'mdi-star-outline' : 'mdi-star']
+                  } />) 
+                }
+                <span>{ Math.round((anime.value.rating / 20) * 10) / 10 }</span>
+              </div>
+              <p><b>Native:</b> { anime.value.native }</p>
+              <p><b>Status:</b> { anime.value.native }</p>
+              <p><b>Studios:</b> { anime.value.native }</p>
+              <p><b>Première diffusion:</b> { anime.value.native }</p>
+              <p><b>Genre(s):</b> { anime.value.native }</p>
             </aside>
           </div>
+          <main>
+          <div class="heading_title">
+              <h1><i class="mdi mdi-comment-text-outline"></i> Synopsis</h1>
+            </div>
+          </main>
         </div>
       </>
     );
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -7,15 +7,15 @@ export default defineComponent({
     return () => (
       <>
         <div class="container">
-        <section class="swiperTop">
-          <Swiper />
-        </section>
+          <section class="swiperTop">
+            <Swiper />
+          </section>
 
-        <section class="page_content">
-          <div class="heading_title">
-            <h1><i class="mdi mdi-diamond-stone"></i>Nos Exclusivités</h1>
-          </div>
-        </section>
+          <section class="page_content">
+            <div class="heading_title">
+              <h1><i class="mdi mdi-diamond-stone"></i>Nos Exclusivités</h1>
+            </div>
+          </section>
         </div>
       </>
     );
